ebaa6980a65fd813b0df889fc103a0e30c3ae7f1
pigbog <mesteves@gmail.com>
2019-01-14 22:13:20 +0000

    added authorization token

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,16 @@
 var request = require('request');
+var token = require('./secrets.js')
 
 console.log('Welcome to Matt\'s Github Avatar Downloader\: ' )
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors", {
+	'user-agent':'request',
+	'authorization':'GITHUB_TOKEN'
+};
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 };
 
 getRepoContributors("jquery", "jquery", function(err, result) {
