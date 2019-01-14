cdb9eff6c4cacaa96cadf2ecf33821b0cb6f2525
pigbog <mesteves@gmail.com>
2019-01-14 22:19:06 +0000

    Now printing the entire JSON

--- download_avatars.js
+++ download_avatars.js
@@ -4,14 +4,17 @@ var token = require('./secrets.js')
 console.log('Welcome to Matt\'s Github Avatar Downloader\: ' )
 
 function getRepoContributors(repoOwner, repoName, cb) {
-var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors", {
-	'user-agent':'request',
-	'authorization':'GITHUB_TOKEN'
-};
-  request(url, function(err, res, body) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
     cb(err, body);
   });
-};
+}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
