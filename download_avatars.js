2bc72b4afe0a451b08d4081724504e1dd6b33020
pigbog <mesteves@gmail.com>
2019-01-15 00:01:53 +0000

    now accepts command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,7 @@
 var request = require('request');
-var token = require('./secrets.js')
+var token = require('./secrets.js');
+var fs = require('fs');
+
 
 console.log('Welcome to Matt\'s Github Avatar Downloader\: ' )
 
@@ -17,8 +19,27 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result, obj) {
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on ('error', function (err){
+  	console.log("Error: ", err )
+  })
+  .pipe(fs.createWriteStream(filePath))
+}
+
+getRepoContributors(process.argv[2], process.argv[3], function(err, result, obj) {
   console.log("Errors:", err);
-  var obj = JSON.parse(result)
+  var obj = JSON.parse(result);
+  var avatarsArr=[]
+  for (i=0; i < obj.length; i++){
+  	downloadImageByURL(obj[i].avatar_url,"./avatars/"+ obj[i].login + ".jpg")
+  }
 });
 
+
+
+
+//"https://api.github.com/repos/"
+///contributors"
+
+///"jquery"
